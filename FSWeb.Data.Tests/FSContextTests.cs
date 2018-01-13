d336300e4df02ee6b8af8d91fb02d32dd9ffbfa8
MudCitizen <mudcitizen@gmail.com>
2018-01-13 11:38:58 -0500

    Added DbInitializer class

--- FSWeb.Data.Tests/FSContextTests.cs
+++ FSWeb.Data.Tests/FSContextTests.cs
@@ -1,4 +1,5 @@
 using FSWeb.Data.DAL;
+using FSWeb.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -20,7 +21,11 @@ namespace FSWeb.Data.Tests
             var builder = new DbContextOptionsBuilder<FSContext>();
             builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=FSWeb;Trusted_Connection=True;MultipleActiveResultSets=true");
             FSContext context = new FSContext(builder.Options);
+            context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
+
+            DbInitializer.Initialize(context);
+        
             Debug.WriteLine("End - " + DateTime.Now.ToShortTimeString());
 
         }
--- FSWeb.Data/DAL/DbInitializer.cs
+++ FSWeb.Data/DAL/DbInitializer.cs
@@ -1,10 +1,61 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using FSWeb.Data.Models;
+
 
 namespace FSWeb.Data.DAL
 {
-    class DbInitializer
+    public static class DbInitializer
     {
+        private static int FindCount = 0; 
+
+        public static void Initialize(FSContext context)
+        {
+            FindCount = 0; 
+            Category mushrooms = new Category() { Name = DbInitializerStrings.CategoryNameMushrooms };
+            context.Add(mushrooms);
+
+            Category wildFlowers = new Category() { Name = DbInitializerStrings.CategoryNameWildflowers };
+            context.Add(wildFlowers);
+
+            Item chantrelle = new Item() { Name = DbInitializerStrings.ItemNameChantrelle, CategoryId = mushrooms.Id };
+            context.Add(chantrelle);
+            Item blackTrumpet= new Item() { Name = DbInitializerStrings.ItemNameBlackTrumpet, CategoryId = mushrooms.Id };
+            context.Add(blackTrumpet);
+            Item trillium = new Item() { Name = DbInitializerStrings.ItemNameTrillium, CategoryId = wildFlowers.Id };
+            context.Add(trillium);
+            Item ladySlipper = new Item() { Name = DbInitializerStrings.ItemNameLadySlipper, CategoryId = wildFlowers.Id };
+            context.Add(ladySlipper);
+
+            // Chantrelle 
+            AddFind(context, DbInitializerStrings.FindNameChantrelle1, chantrelle.Id);
+            AddFind(context, DbInitializerStrings.FindNameChantrelle2, chantrelle.Id);
+
+
+            // Black Trumpet
+            AddFind(context, DbInitializerStrings.FindNameBlackTrumpet1, blackTrumpet.Id);
+            AddFind(context, DbInitializerStrings.FindNameBlackTrumpet2, blackTrumpet.Id);
+
+            // Trillium
+            AddFind(context, DbInitializerStrings.FindNameTrillium1, trillium.Id);
+            AddFind(context, DbInitializerStrings.FindNameTrillium2, trillium.Id);
+
+            // LadySlipper
+            AddFind(context, DbInitializerStrings.FindNameLadySlipper1, ladySlipper.Id);
+            AddFind(context, DbInitializerStrings.FindNameLadySlipper2, ladySlipper.Id);
+
+            context.SaveChanges();
+
+        }
+
+        static void AddFind(FSContext context, String name, int itemId)
+        {
+            FindCount++;
+            DateTime date = DateTime.Now.AddMonths(-1 * (FindCount));
+            context.Finds.Add(new Find() { Name = name, ItemId = itemId, Date = date });
+
+        }
     }
 }
