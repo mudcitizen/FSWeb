d59ca0b3fff72e8815f56d9fcedac56b185d2c86
MudCitizen <mudcitizen@gmail.com>
2018-01-16 20:21:59 -0500

    Added ItemSummary

--- FSWeb.Data.Tests/FSContextTests.cs
+++ FSWeb.Data.Tests/FSContextTests.cs
@@ -3,6 +3,7 @@ using FSWeb.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace FSWeb.Data.Tests
@@ -11,21 +12,26 @@ namespace FSWeb.Data.Tests
     public class FSContextTests
     {
 
+        FSContext DbContext; 
         [TestInitialize]
         public void TestInitializer() {
             Debug.WriteLine("TestInitializer()");
-        }
-        [TestMethod]
-        public void TestCreate()
-        {
             var builder = new DbContextOptionsBuilder<FSContext>();
             builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=FSWeb;Trusted_Connection=True;MultipleActiveResultSets=true");
-            FSContext context = new FSContext(builder.Options);
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
+            DbContext = new FSContext(builder.Options);
+            DbContext.Database.EnsureDeleted();
+            DbContext.Database.EnsureCreated();
 
-            DbInitializer.Initialize(context);
-        
+            DbInitializer.Initialize(DbContext);
+        }
+        [TestMethod]
+        public void TestItemSummary()
+        {
+            Debug.WriteLine(String.Format("TestItemSummary() - Begin - {0}", DateTime.Now.ToLongTimeString()));
+            foreach (ItemSummary itemSummary in DbContext.ItemSummaries)
+                Debug.WriteLine(itemSummary.ToString());
+            Debug.WriteLine(String.Format("TestItemSummary() - End   - {0}", DateTime.Now.ToLongTimeString()));
+
             Debug.WriteLine("End - " + DateTime.Now.ToShortTimeString());
 
         }
--- FSWeb.Data/DAL/FSContext.cs
+++ FSWeb.Data/DAL/FSContext.cs
@@ -1,6 +1,7 @@
 ﻿using FSWeb.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -15,6 +16,37 @@ namespace FSWeb.Data.DAL
         public DbSet<Category> Categories { get; set; }
         public DbSet<Find> Finds { get; set; }
 
+        public IEnumerable<ItemSummary> ItemSummaries
+        {
+            get
+            {
+                IEnumerable<FindSummary> findSummary = Finds.GroupBy(f => f.ItemId).Select(grouping => new FindSummary()
+                { ItemId = grouping.Key, LastFind = grouping.Max(f => f.Date), FindCount = grouping.Count() }).ToList();
+
+                var itemSummary = Items
+                    .Join(Categories, item => item.CategoryId, cat => cat.Id, (item, cat) =>
+                     new
+                     {
+                         ItemId = item.Id,
+                         ItemName = item.Name,
+                         CategoryName = cat.Name,
+                         CategoryId = cat.Id
+                     }
+                     )
+                     .Join(findSummary, item => item.ItemId, fs => fs.ItemId, (item, fs) => new ItemSummary(item.CategoryName, item.CategoryId, item.ItemName, item.ItemId, fs.FindCount, fs.LastFind));
+
+                /*
+                var itemSummary2 = Items
+                       .Join(findSummary, item => item.Id, 
+                             fs => fs.ItemId, 
+                             (item, fs) => new ItemSummary(item.Category.Name, item.Category.Id, item.Name, item.Id, fs.FindCount, fs.LastFind));
+                             */
+
+                return itemSummary;
+            }
+
+        }
+
         public FSContext(DbContextOptions<FSContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -23,6 +55,13 @@ namespace FSWeb.Data.DAL
             modelBuilder.Entity<Item>().ToTable(Contants.TableNames.Items);
             modelBuilder.Entity<Find>().ToTable(Contants.TableNames.Finds);
             base.OnModelCreating(modelBuilder);
+        }
+
+        class FindSummary
+        {
+            public int ItemId { get; set; }
+            public int FindCount { get; set; }
+            public DateTime LastFind { get; set; }
         }
 
     }
