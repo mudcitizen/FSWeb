75b482f949a12dea6bb508badb67899130c6d516
MudCitizen <mudcitizen@gmail.com>
2018-01-28 11:12:44 -0500

    Pagination with TagHelper

--- FSWeb/Controllers/HomeController.cs
+++ FSWeb/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FSWeb.Data.Models;
+using FSWeb.Models.ViewModels;
 using FSWeb.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,14 +12,28 @@ namespace FSWeb.Controllers
     public class HomeController : Controller
     {
         IRepository repository;
+        int PageSize = 2;
 
         public HomeController(IRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index()
+        public IActionResult Index(int pageNumber = 1)
         {
-            return View(repository.ItemSummaries);
+            HomeIndexViewModel vm = new HomeIndexViewModel();
+
+            vm.ItemSummaries = repository.ItemSummaries
+                .OrderBy(i => i.CategoryName)
+                .OrderBy(i => i.ItemName)
+                .Skip((pageNumber - 1) * PageSize)
+                .Take(PageSize);
+
+            vm.PagingInfo = new PagingInfo();
+            vm.PagingInfo.TotalItems = repository.ItemSummaries.Count();
+            vm.PagingInfo.ItemsPerPage = PageSize;
+            vm.PagingInfo.CurrentPage = pageNumber;
+
+            return View(vm);
         }
     }
 }
---
+++ FSWeb/Models/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FSWeb.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSWeb.Models.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+       public PagingInfo PagingInfo { get; set; }
+       public IEnumerable<ItemSummary> ItemSummaries { get; set; }
+
+    }
+}
