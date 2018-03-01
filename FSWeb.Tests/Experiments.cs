816ac1d975c6cd1b1e74f618918a2e3e2c3df7ec
MudCitizen <mudcitizen@gmail.com>
2018-02-28 19:25:05 -0500

    Experiments

---
+++ FSWeb.Tests/Experiments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Configuration;
+
+namespace FSWeb.Tests
+{
+
+    [TestClass]
+    public class Experiments
+    {
+        [TestMethod]
+        public void TestDateToString()
+        {
+            DateTime dt = DateTime.Now;
+            String dts = dt.ToString("d");
+            Debug.WriteLine(String.Format("{0} -  {1}", dt, dts));
+            Debug.WriteLine("Here boss");
+
+        }
+    }
+}
