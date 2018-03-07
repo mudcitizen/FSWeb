9ef3e53656f05d3c79f4f31b0f41824a01473ba3
MudCitizen <mudcitizen@gmail.com>
2018-03-06 20:40:53 -0500

    Select list for Category

--- FSWeb/Controllers/ItemController.cs
+++ FSWeb/Controllers/ItemController.cs
@@ -1,7 +1,10 @@
-﻿using FSWeb.Models;
+﻿using System.Linq;
+using System.Collections.Generic;
+using FSWeb.Models;
 using FSWeb.Models.ViewModels;
 using FSWeb.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FSWeb.Controllers
 {
@@ -16,10 +19,14 @@ namespace FSWeb.Controllers
 
         public IActionResult Display(int id)
         {
+
+            List<SelectListItem> categorySelector = new List<SelectListItem>();
+            foreach (CategoryUI cat in repo.GetAllCategories().OrderBy(c => c.Name)) {
+                categorySelector.Add(new SelectListItem() { Value = cat.Id.ToString(), Text = cat.Name });
+            }
             ItemUI item = repo.GetItem(id);
             CategoryUI category = repo.GetCategory(item.CategoryId);
-
-            ItemVM vm = new ItemVM() { Item = item, Category = category };
+            ItemVM vm = new ItemVM() { Item = item, Category = category, CategorySelector = categorySelector };
             return View(vm);
         }
     }
--- FSWeb/Models/ViewModels/ItemVM.cs
+++ FSWeb/Models/ViewModels/ItemVM.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FSWeb.Models.ViewModels
 {
@@ -9,5 +10,6 @@ namespace FSWeb.Models.ViewModels
     {
         public ItemUI Item { get; set; }
         public CategoryUI Category { get; set; }
+        public List<SelectListItem> CategorySelector { get; set; }
     }
 }
